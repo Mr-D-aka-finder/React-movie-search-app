0455e1153b03e5b32c22cc3c2f0d3467b988dc30
D <d.hirohata@icloud.com>
2022-09-16 12:16:51 +0900

    feat: App.cssファイルの編集

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
+import React from "react";
 import "../App.css";
 
 const App = () => {
-  <div className="App">
-    <p>Hello React</p>
-  </div>;
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+    </div>
+  );
 };
 
 export default App;
